9542ced9a45d77f2c7697fffb2685f6025497a11
Sahil Saini <sahilsainimani8398@gmail.com>
2025-12-20 15:51:57 +0530

    patch

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import SectionThree from "./sections/SectionThree";
 import SectionTwo from "./sections/SectionTwo";
 import SectionNine from "./sections/SectionNine";
 import Footer from "./sections/Footer";
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   const [screenWidth, setScreenWidth] = useState(window.innerWidth);
@@ -24,7 +24,7 @@ function App() {
   });
   return (
     <div className="h-screen bg-surface flex flex-col select-none duration-150 ease-in **:duration-150 **:ease-in">
-      <Analytics/>
+      <Analytics />
       <Navbar />
       <Hero />
       <div>
@@ -38,6 +38,16 @@ function App() {
       <SectionEight />
       <SectionNine screenWidth={screenWidth} />
       <Footer screenWidth={screenWidth} />
+      <div className="p-2 w-full flex items-center justify-center text-sm gap-2 bg-black text-white">
+        <p>Original Website: </p>
+        <a
+          href="https://www.kickgame.com/?shpxid=2dfc2414-6f7e-477b-b614-425607d1aa79"
+          target="_blank"
+          className="underline"
+        >
+          Kickgame.com
+        </a>
+      </div>
     </div>
   );
 }
